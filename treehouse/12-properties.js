b0f85aaf19d10ece2d1476d903f98b39adb157e0
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-23 14:58:29 -0400

    Create 12-properties.js

---
+++ treehouse/12-properties.js
@@ -0,0 +1,46 @@
+//React Props
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Card(props) {
+  //create React props, works like custom html element that you can create your own attribute, if any html attribute only adds here
+  return (
+    <div className='only-works-here'>
+      <h2>{props.name}</h2>
+      <img src={props.img} alt="avatar_img" />
+      <p>{props.tel}</p>
+      <p>{props.email}</p>
+    </div>
+  );
+} //inside html interpret as javascript so use {}
+
+ReactDOM.render(    //using React props above to create as many cards as you need with customized information
+  <div>
+    <h1>My Contacts</h1>
+    <Card
+      name="Beyonce"
+      img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+      tel="+123 456 789"
+      email="b@beyonce.com"
+    />
+    <Card
+      name="Chuck Norris"
+      img="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
+      tel="+918 372 574"
+      email="gmail@chucknorris.com"
+    />
+    <Card
+      name="Jack Bauer"
+      img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+      tel="+987 654 321"
+      email="jack@nowhere.com"
+    />
+  </div>,
+  document.getElementById("root")
+);
+
+
+
+//React Props Practice
+
