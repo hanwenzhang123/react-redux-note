d57b11e918d76c3b89ec58fe63e6b2e055c6b6cb
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-23 09:43:44 -0400

    Create 00-setup.js

---
+++ app-brewery/00-setup.js
@@ -0,0 +1,16 @@
+https://reactjs.org/docs/create-a-new-react-app.html
+
+npx create-react-app my-app
+cd my-app
+npm start
+
+ReactDOM.render(what to show, where to show it);
+
+
+<!-- Load React. -->
+<!-- Note: when deploying, replace "development.js" with "production.min.js". -->
+<script src="https://unpkg.com/react@16/umd/react.development.js" crossorigin></script>
+<script src="https://unpkg.com/react-dom@16/umd/react-dom.development.js" crossorigin></script>
+
+<!-- Load our React component. -->
+<script src="../src/index.js"></script>
