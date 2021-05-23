9e646eec2c9386ba9afa1a06bf8c0c7c42837bc9
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-23 10:45:44 -0400

    Update 06-import-export-modules.js

--- app-brewery/06-import-export-modules.js
+++ app-brewery/06-import-export-modules.js
@@ -1,9 +1,37 @@
 //Javascript ES6 - Import, Export and Modules
 
 
+//index.js
+import React from "react";
+import ReactDOM from "react-dom";
+import pi, { doublePi, triplePi } from "./math.js";
+
+ReactDOM.render(
+  <ul>
+    <li>{pi}</li>
+    <li>{doublePi()}</li>
+    <li>{triplePi()}</li>
+  </ul>,
+  document.getElementById("root")
+);
 
 
+//math.js
+const pi = 3.1415962;
+
+function doublePi() {
+  return pi * 2;
+}
+
+function triplePi() {
+  return pi * 3;
+}
+
+export default pi;
+export { doublePi, triplePi };
 
 
 
 //Javascript ES6 Import, Export and Modules Practice
+
+
