a728e2dfaf7d57c90ee724da468f19d4b8c49e6f
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-23 10:24:21 -0400

    Update 00-setup.js

--- app-brewery/00-setup.js
+++ app-brewery/00-setup.js
@@ -1,11 +1,39 @@
 //https://reactjs.org/docs/create-a-new-react-app.html
-//https://reactjs.org/docs/add-react-to-a-website.html#add-react-in-one-minute
+
+//https://create-react-app.dev/docs/getting-started/
+
+//npm uninstall -g create-react-app
 
 npx create-react-app my-app
 cd my-app
 npm start
 
-ReactDOM.render(what to show, where to show it);
+Then open http://localhost:3000/ to see your app
+
+
+
+//index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>Hello World!</h1>,
+  document.getElementById('root')
+);
+
+//index.html
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>React App</title>
+  </head>
+  <body>
+    <div id="root"></div>
+    <script src="../src/index.js"></script>
+  </body>
+</html>
+
+
 
 
 <!-- Load React. -->
