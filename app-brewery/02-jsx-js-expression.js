e425b61433d2b682d69566de4a4c99d954c9a714
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-22 21:58:06 -0400

    Update 02-jsx-js-expression.js

--- app-brewery/02-jsx-js-expression.js
+++ app-brewery/02-jsx-js-expression.js
@@ -17,4 +17,20 @@ document.getElementById("root")
 );
 
 
+
+ //Javascript Expressions in JSX Practice
+import React from "react";
+import ReactDOM from "react-dom";
+
+const name = 'Hanwen';
+const year = new Date().getFullYear();
+
+ReactDOM.render(
+<div>
+  <p>Created by {name}!</p>
+  <p>Copyright {year}.</p>
+</div>, 
+document.getElementById("root")
+);
+
  
