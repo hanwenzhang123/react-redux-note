44fd098998988617393661a0c1ceed93b6aef627
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-23 11:02:06 -0400

    Update 06-import-export-modules.js

--- app-brewery/06-import-export-modules.js
+++ app-brewery/06-import-export-modules.js
@@ -43,4 +43,56 @@ export { doublePi, triplePi };    //non-default export
 
 //Javascript ES6 Import, Export and Modules Practice
 
+//index.js
+import React from "react";
+import ReactDOM from "react-dom";
+import { add, multiply, subtract, divide } from "./calculator.js";
+//order does not matter but names matter.
 
+ReactDOM.render(
+  <ul>
+    <li>{add(1, 2)}</li>
+    <li>{multiply(2, 3)}</li>
+    <li>{subtract(7, 2)}</li>
+    <li>{divide(5, 2)}</li>
+  </ul>,
+  document.getElementById("root")
+);
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import * as Cals from "./calculator.js";
+// //alernative wildcard way
+
+// ReactDOM.render(
+//   <ul>
+//     <li>{Cals.add(1, 2)}</li>
+//     <li>{Cals.multiply(2, 3)}</li>
+//     <li>{Cals.subtract(7, 2)}</li>
+//     <li>{Cals.divide(5, 2)}</li>
+//   </ul>,
+//   document.getElementById("root")
+// );
+
+
+//calculator.js
+function add(n1, n2) {
+  return n1 + n2;
+}
+
+function multiply(n1, n2) {
+  return n1 * n2;
+}
+
+function subtract(n1, n2) {
+  return n1 - n2;
+}
+
+function divide(n1, n2) {
+  return n1 / n2;
+}
+
+//export first before import elsewhere
+export { add, multiply, subtract, divide };
+  
+ 
