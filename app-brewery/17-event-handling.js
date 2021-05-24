de0652d8960a7aa87e058a08fdca08549fdfde07
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-24 19:40:45 -0400

    Create 17-event-handling.js

---
+++ app-brewery/17-event-handling.js
@@ -0,0 +1,45 @@
+HTML Attributes
+https://www.w3schools.com/html/html_attributes.asp
+
+
+//App.jsx
+import React, { useState } from "react";
+
+function App() {
+  const [headingText, setHeadingText] = useState('Hello');
+  // [ h1 tag,  function (setState) ] = useState(initial value at the first item here is h1 tag)
+
+  const [isMouseOver, setMouseOver] = useState(false);
+  // isMouseOver initially set as false, then setMouseOver function changes it
+
+  function handleClick() {
+    setHeadingText('Submitted');
+  }
+
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
+
+  function handleMouseOut() {
+    setMouseOver(false);
+  }
+
+  return (
+    <div className="container">
+      <h1>{headingText}</h1>
+      <input type="text" placeholder="What's your name?" />
+      <button style={{ backgroundColor: isMouseOver ? "black" : "White"}} 
+      onClick={handleClick}
+      onMouseOver = {handleMouseOver}
+      onMouseOut = {handleMouseOut}
+      >
+        Submit
+        </button>
+    </div>
+  );
+}
+
+export default App;
+
+  //because we are in javascript file, so all the attributes use camelCase like backgroundColor and onClick. 
+//if isMouseOver is true, we set the backgroundColor to true, so it will be black, else if it is true, it will be white
