bde14ca5715b543f79254879d32cda69bb36ace4
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-23 20:32:27 -0400

    Create 09-mapping-components.js

---
+++ app-brewery/09-mapping-components.js
@@ -0,0 +1,7 @@
+Common uses for this element are to implement a glossary or to display metadata (a list of key-value pairs).
+<dl> element represents a description list. 
+<dt> a list of groups of terms
+<dd> a list of groups of descriptions 
+
+
+  
