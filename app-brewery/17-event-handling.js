fd6b009fc695169b1de6d0fbdebd05e8b8922a52
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-24 19:52:39 -0400

    Update 17-event-handling.js

--- app-brewery/17-event-handling.js
+++ app-brewery/17-event-handling.js
@@ -43,3 +43,18 @@ export default App;
 
   //because we are in javascript file, so all the attributes use camelCase like backgroundColor and onClick. 
 //if isMouseOver is true, we set the backgroundColor to true, so it will be black, else if it is true, it will be white
+
+//<button style={{ backgroundColor: isMouseOver ? "black" : "White"}}> 
+- one set of {} - within JSX {variableName}, braces are needed to reference any JS variable or expression - e.g. style={divStyle}
+- one set of {} - passing in as javascript object, creating an object
+
+style={
+  {
+    backgroundImage: `url(${avatarURL})`,
+    color: #ffffff,
+    fontSize: 16px
+  }
+}
+
+outer {} brackets are for returning a variable, and the inner {} brackets are for creating an object.
+  
