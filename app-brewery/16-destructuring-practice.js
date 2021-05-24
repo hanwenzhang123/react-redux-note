78f5398301d8e6bbd056a1304024565cb67743e5
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-24 19:09:35 -0400

    Create 16-destructuring-practice.js

---
+++ app-brewery/16-destructuring-practice.js
@@ -0,0 +1,65 @@
+//index.js
+import React from "react";
+import ReactDOM from "react-dom";
+import cars from "./practice";
+
+const [honda, tesla] = cars;
+
+const {
+  speedStats: { topSpeed: hondaTopSpeed }   //object
+} = honda;
+const {
+  speedStats: { topSpeed: teslaTopSpeed }   //object
+} = tesla;
+
+const {
+  coloursByPopularity: [hondaTopColour]   //array, the 1st value
+} = honda;
+const {
+  coloursByPopularity: [teslaTopColour]   //array, the 1st value
+} = tesla;
+
+ReactDOM.render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+      <th>Top Colour</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td> 
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
+    </tr>
+  </table>,
+  document.getElementById("root")
+);
+
+
+//practice.js
+const cars = [
+  {
+    model: "Honda Civic",
+    coloursByPopularity: ["black", "silver"],
+    speedStats: {
+      topSpeed: 140,
+      zeroToSixty: 8.5
+    }
+  },
+  {
+    model: "Tesla Model 3",
+    coloursByPopularity: ["red", "white"],
+    speedStats: {
+      topSpeed: 150,
+      zeroToSixty: 3.2
+    }
+  }
+];
+
+export default cars;
+  
