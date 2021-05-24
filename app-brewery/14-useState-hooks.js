ef1f97976b7aaab120f1e901e6a62a4c690fd44a
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-24 16:21:30 -0400

    Create 14-useState-hooks.js

---
+++ app-brewery/14-useState-hooks.js
@@ -0,0 +1,60 @@
+//https://reactjs.org/docs/hooks-reference.html#usestate
+Rule: must use hooks inside of a function component
+
+const [red, green, blue] = [9, 132, 227];
+console.log(blue) //277
+
+const state = useState(123);
+console.log(state)  //[123{initial value}, function];
+
+
+
+//useState - The setState function is used to update the state.
+
+const [state, setState] = useState(initialState);
+
+setState(newState);
+
+
+function Counter({initialCount}) {
+  const [count, setCount] = useState(initialCount);
+  return (
+    <>
+      Count: {count}
+      <button onClick={() => setCount(initialCount)}>Reset</button>
+      <button onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
+      <button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
+    </>
+  );
+}
+
+
+//APP.jsx - counting
+import React, { useState } from "react";    //useState from the React module
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  function increase() {   //count is a current value in ()
+    setCount(count + 1);
+  }
+
+  function decrease() {
+    setCount(count - 1);
+  }
+
+  return (
+    //output
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={decrease}>-</button>
+      <button onClick={increase}>+</button>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+//useState Hook Practice
