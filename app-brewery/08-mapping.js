7da4d81a8498f1523602baed3c71705c003ac134
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-23 20:29:50 -0400

    Create 08-mapping.js

---
+++ app-brewery/08-mapping.js
@@ -0,0 +1,31 @@
+//mapping is very useful especially handling arrays
+
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
+
+function createCard(contact) {    //key is not for use, we need to create a new 'id={contact.id}' if we want to access the data
+  return (
+    <Card
+      key={contact.id}
+      name={contact.name}
+      img={contact.imgURL}
+      tel={contact.phone}
+      email={contact.email}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      {contacts.map(createCard)}
+    </div>
+  );
+}
+
+export default App;
+
+
+  
