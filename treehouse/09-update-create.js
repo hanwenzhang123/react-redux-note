90b8102efdc3c3a939b9b3b9f984405b2c68f1c5
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-25 00:49:35 -0400

    Update 09-update-create.js

--- treehouse/09-update-create.js
+++ treehouse/09-update-create.js
@@ -1,6 +1,6 @@
 //Update State Based on Previous State
 
-Whenever you need to update state based on previous state, you shouldn't rely on this.state to calculate the next state.
+Whenever you need to update state based on previous state, you should not rely on this.state to calculate the next state.
 State updates may be asynchronous, so it may not always lead to the component re-rendering with new data, and could cause state inconsistency. 
 setState() accepts a callback function that produces state based on the previous state in a more reliable way.
 
