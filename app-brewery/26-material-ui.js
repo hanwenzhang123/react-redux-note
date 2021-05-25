267f8015f91eba9eb88dd54f46809a3a1b000bd2
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-25 17:04:36 -0400

    Create 26-material-ui.js

---
+++ app-brewery/26-material-ui.js
@@ -0,0 +1,21 @@
+Material UI Icons
+https://www.npmjs.com/package/@material-ui/icons
+
+Material UI Core
+https://www.npmjs.com/package/@material-ui/core
+
+
+Material UI Icons Documentation
+https://material-ui.com/components/icons/
+
+
+Material UI Floating Action Buttons
+https://material-ui.com/components/buttons/#floating-action-buttons
+
+
+Zoom API
+https://material-ui.com/api/zoom/
+
+
+Transparent Textures
+https://www.transparenttextures.com/
