f7f72d4d63bb748d940f8f3cc096f4f774860bd0
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-06-07 13:11:53 -0400

    Update 10-create-state.js

--- treehouse/10-create-state.js
+++ treehouse/10-create-state.js
@@ -1,6 +1,13 @@
-//Creating the Application State
-  // Data that is avaiable the entire application
-  //state  is a an object that stores all the data that the component itself needs and data that might get passed down to its children.
+Creating the Application State
+Data that is avaiable the entire application
+state is a an object that stores all the data that the component itself needs and data that might get passed down to its children.
+
+
+Types of State
+- Application State: main state - sata that is avaiable the entire application
+- Component State - state that is specific to a component and not shared outside of the component, like a local state
+
+
 
 class App extends React.Component {
 
@@ -53,10 +60,6 @@ ReactDOM.render(
   
 //component State
  state that is specific to a componen and not shared outside of the component
- 
  class Counter extends React.Component { }
 
-
-
-
-     
+  
