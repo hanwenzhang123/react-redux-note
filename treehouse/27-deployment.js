c6c99bb26b089c9e3eea02324d271dfba2256450
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-06-10 12:20:50 -0400

    Create 27-deployment.js

---
+++ treehouse/27-deployment.js
@@ -0,0 +1,45 @@
+//local
+// install react cli
+npm install -g create-react-app
+
+// create app
+create-react-app my-react-app-name
+
+// go to project folder
+cd my-react-app-name
+
+// install dependencies
+npm install
+
+// start live server
+npm start
+
+npm run-script start
+npm run start
+npm start
+
+
+//Github
+npm install gh-pages --save -dev
+
+//Package.json
+
+"homepage": "https://hanwenzhang123.github.io/{repo-name}",
+    
+"scripts": {
+"predeploy": "npm run build",
+"deploy": "gh-pages -d build",
+  }
+
+
+git init
+git add .
+git commit -m ' '
+git remote add origin
+
+npm run deploy
+
+
+//Deploy to Now
+//Deploy to netlify
+  
