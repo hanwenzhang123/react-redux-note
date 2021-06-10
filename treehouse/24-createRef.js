8cf79d10e84544a403d322e16ce42da853757165
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-06-10 00:25:04 -0400

    Create 24-createRef.js

---
+++ treehouse/24-createRef.js
@@ -0,0 +1,5 @@
+//Refs and the DOM 
+ Refs let you access and interact with DOM nodes created in the render() method. 
+ They make it possible to do the more traditional DOM manipulation, and they're commonly used to access form elements and get their values.
+
+
