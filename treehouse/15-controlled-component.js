ac03145dac494839fd46b29f43701c5975e914bc
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-06-09 21:02:54 -0400

    Create 15-controlled-component.js

---
+++ treehouse/15-controlled-component.js
@@ -0,0 +1,8 @@
+To manage an input field's state, we need to build a "controlled component." 
+A controlled component renders a form element whose values are controlled by React, with state.
+
+
+create a controlled component
+- initialize state for the value of the input
+- listen for changes on the input to detect when value is updated
+- create an event handler that updates the value state
