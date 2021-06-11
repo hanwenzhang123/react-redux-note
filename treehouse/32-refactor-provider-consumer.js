121616c14355b2c2108e0e4a5c4d0b152fe0cc4a
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-06-10 22:25:34 -0400

    Create 32-refactor-provider-consumer.js

---
+++ treehouse/32-refactor-provider-consumer.js
@@ -0,0 +1,11 @@
+//Provide and Consume Actions
+It's common to pass the Provider's value prop an object to store multiple properties in state, as well as any actions you want to perform on the data.
+
+//Refactor the Provider
+Higher-order component(HOC)
+A technique in React for reusing component logic. A HOC takes an existing component and returns a new component. 
+
+children
+A speial react prop that let you pass components as data to other components
+
+//Refactor the Consumers
