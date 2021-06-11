47e9b094ac2e13577d831c7c14e344cb2b7a7a80
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-06-10 21:04:18 -0400

    Update 31-create-context.js

--- treehouse/31-create-context.js
+++ treehouse/31-create-context.js
@@ -113,3 +113,67 @@ Stats.propTypes = {
 export default Stats;
 
   
+//Playerlist.js(new)
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Consumer } from './Context';
+import Player from './Player';
+
+const PlayerList = (props) => {
+  return (
+    <Consumer>
+      { context => (  //react.fragment groups a list a sibling elements or components without having to add an unnecessary reaper element
+        <React.Fragment>
+          {context.map( (player, index) =>
+            <Player 
+              {...player}   //name, id, score in the player.js
+              key={player.id.toString()} 
+              index={index}
+              changeScore={props.changeScore}
+              removePlayer={props.removePlayer}           
+            />
+          )}
+        </React.Fragment>
+      )}
+    </Consumer>
+  );
+}
+
+PlayerList.propTypes = {
+  changeScore: PropTypes.func.isRequired,
+  removePlayer: PropTypes.func.isRequired,
+};
+
+export default PlayerList;
+
+
+
+//Playerlist.js(old)
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import Player from './Player';
+
+const PlayerList = (props) => {
+  return (
+    <React.Fragment>
+      {props.players.map( (player, index) =>
+        <Player 
+          {...player}
+          key={player.id.toString()} 
+          index={index}
+          changeScore={props.changeScore}
+          removePlayer={props.removePlayer}           
+        />
+      )}
+    </React.Fragment>
+  );
+}
+
+PlayerList.propTypes = {
+  players: PropTypes.arrayOf(PropTypes.object),
+  changeScore: PropTypes.func.isRequired,
+  removePlayer: PropTypes.func.isRequired,
+};
+
+export default PlayerList;
