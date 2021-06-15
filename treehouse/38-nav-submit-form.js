c4adac4fcacdfd054d8fd66a9729118184d66700
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-06-15 17:10:25 -0400

    Update 38-nav-submit-form.js

--- treehouse/38-nav-submit-form.js
+++ treehouse/38-nav-submit-form.js
@@ -40,3 +40,22 @@ class Home extends Component {
 export default Home;
 
   
+
+//Review
+The ____ object can be used to programmatically change the URL.
+history
+
+Which component renders only the first <Route> that matches the URL and helps create 404-like routes?
+  <Switch>
+  
+Given this route:
+<Route path="dogs/:breed" component={Dogs} />
+How do you access the value of the code>:breed parameter in a component?
+  match.params.breed
+  
+You can give a URL parameter any name, as long as you include a __ in front of it.
+:
+  
+Is this a valid route declaration?
+<Route component={Footer} />
+Yes. This renders the Footer component no matter which URL path is active.
