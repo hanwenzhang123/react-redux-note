b44a29fb1c36c34aef301b7eb0493f78e5cab212
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-06-19 23:05:02 -0400

    Create 40-componentDidCatch.js

---
+++ treehouse/40-componentDidCatch.js
@@ -0,0 +1,2 @@
+With componentDidCatch() comes a new concept of an error boundary. 
+Error boundaries are wrapper components that use componentDidCatch() to capture errors anywhere in their child component tree and display a fallback UI.
