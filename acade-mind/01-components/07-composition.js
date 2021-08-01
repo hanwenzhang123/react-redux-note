efe90832a873674ae31e0ff18d1b454c94ef163c
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-08-01 19:34:09 -0400

    Update 07-composition.js

--- acade-mind/01-components/07-composition.js
+++ acade-mind/01-components/07-composition.js
@@ -1,24 +1,25 @@
 // The concept of composition (children props)
 
-// Props for configuring these components for passing data int them
-// Components are just these custom HTML elements where you combine JSX code and styling
+// Components are these custom HTML elements where you combine JSX code and styling CSS
 
 // We build all these components to then build a user interface
 // In App.js we are using Expenses in there we are using ExpenseItem in there we're using ExpenseDate.
-
-// Generally, this approach of building a user interface from smaller building blocks is called composition.
+// The approach of building a user interface from smaller building blocks is called composition.
 
 // What if we wanted to create a component which actually just serves as a shell around any kind of other content?
-// we create a Card component
+// you wanna have a component where you do not configure everything through props
+// but where instead you are able to pass content between the opening and closing tags of that component.
+// we create a Card component here as an example
 
-//
+
+//ExpenseItem.js
 import ExpenseDate from './ExpenseDate';
 import Card from './Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
   return (
-    <Card className='expense-item'>
+    <Card className='expense-item'>   {/*custom component*/}
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
@@ -30,18 +31,23 @@ function ExpenseItem(props) {
 
 export default ExpenseItem;
 
-//Card.js
+
+//Card.js - container look with rounded corners, drop shadows and elements like these
 import './Card.css';
 
 function Card(props) {
-  const classes = 'card ' + props.className;
-  
+  const classes = 'card ' + props.className;  //card as the default class that always applied
+                       //props.className - anything received as a class name from outside is added to that string
   return <div className={classes}>{props.children}</div>;
 }
 
 export default Card;
 //children is the reserved name
+//the value of children prop will always be the content between the opening and closing tags of your custom component.
 
+//Whenever you combine components, you are using composition.
+//We compose our ExpenseItem component by using card as a wrapper, by using some built-in HTML elements, and by then all the putting in the ExpenseDate.
+    
 
 //Card.css
 .card {
@@ -49,4 +55,4 @@ export default Card;
   box-shadow: 0 1px 8px rgba(0, 0, 0, 0.25);
 }
 
-  
+   
