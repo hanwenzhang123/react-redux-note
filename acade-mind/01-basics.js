ac35dcc8c7c09958acd4fd65edd118850a75ed78
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-31 21:17:39 -0400

    Update 01-basics.js

--- acade-mind/01-basics.js
+++ acade-mind/01-basics.js
@@ -50,7 +50,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));    //we would only render once for our root component in this file
 
 
 //index.html - only 1 html file
