bf7dcdfb3bc5b1371858c7a100c1381667328097
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-08-01 14:18:24 -0400

    Update 04-dynamic-props.js

--- acade-mind/04-dynamic-props.js
+++ acade-mind/04-dynamic-props.js
@@ -102,17 +102,9 @@ export default App;
 import "./ExpenseItem.css";
 
 function ExpressItem(props) {
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date.getFullYear(); //return the 4-digit number year
-
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
@@ -126,3 +118,4 @@ export default ExpressItem;
 //Props is an important concept because it allows you to make your components reusable,
 //and it allows you to pass data from another component to this component 
     
+   
