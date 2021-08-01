081c090be7e050108f925b95577201e6a2e37ad5
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-31 20:53:35 -0400

    Update 01-components.js

--- acade-mind/01-components.js
+++ acade-mind/01-components.js
@@ -22,11 +22,12 @@ All React components are building a combination of all above, mainly HTML and JS
 
 React allows you to create re-usable and reactive components consisting of HTML and JS (and CSS)
 Declaretive Approach - using a specific way
-Defind the desired target state(s) and let Reaact figure out the actual JS DOM instructions
+Defind the desired target state(s) and let React figure out the actual JS DOM instructions
   
 React is a single-page application which means only one HTML file 
 that is delivered to the browser and hosted by the broswer and rendered by the browser
 we use React to do user interfaces to modify that single page
+
 
 //this is the first file to be execute
 //index.js
@@ -37,13 +38,15 @@ import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+
 //index.html - only 1 html file
 <div id="root"></div> 
 //targeted by document.getElementById('root'), which will be replaced by <App />
 
-//App.js
+
+/*App.js*/
 function App() {
-  return (      //JSX below
+  return (      //JSX below - JS XML, because HTML is XML in the end
     <div>
       <h2>Let's get started!</h2>
     </div>
@@ -52,3 +55,4 @@ function App() {
 
 export default App;
  
+   
