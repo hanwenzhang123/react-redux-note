c054b1ea6ee093cf82f453da3ee9b6f86e2a72b1
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-08-01 20:44:09 -0400

    Update 01-state.js

--- acade-mind/02-state/01-state.js
+++ acade-mind/02-state/01-state.js
@@ -1,11 +1,13 @@
 User Interaction & State
 Making Apps Interactive and Reactive (change when something happens)
 
-
 //ExpenseItem.js
-import React, { useState } from "react"; // import React overall object but only used once when we call it
+// import React overall object but only used once when we call it
 //to call something to run again, we need to add named-import
 //useState is a react hook, only be called inside react function
+//useState always get back two values, the value itself and the updating function
+
+import React, { useState } from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
@@ -14,13 +16,12 @@ import "./ExpenseItem.css";
 const ExpenseItem = (props) => {
   const [title, setTitle] = useState(props.title); //array destructuring, current state value + function updating the value
 
-  const clickHandler = () => {
+  const clickHandler = () => { //state on a per component instance basis, we have separate states, so no interrupts among states
     setTitle("Updated!");
     console.log(title);
-  }; //execute this function when the event occur so inside the onClick no ()
-
-  //React component is just a function, only difference is it returns JSX
-  return (
+  }; 
+  
+ return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
@@ -29,11 +30,13 @@ const ExpenseItem = (props) => {
       </div>
       <button onClick={clickHandler}>Change Title</button>
     </Card>
-  );
+  );    //execute the clickHandler function when the event occur so inside the onClick no ()
 };
 
 export default ExpenseItem;
 
+//React component is just a function, only difference is it returns JSX
 //We have all the DOM button element listening events we can use
 //however, React starts with on like onClick
    
+ 
