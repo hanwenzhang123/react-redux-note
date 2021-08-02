503a731b5fd6106bbdf9bffe56f20fbfc776dc05
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-08-01 22:10:42 -0400

    Create 02-listening-input.js

---
+++ acade-mind/02-state/02-listening-input.js
@@ -0,0 +1,54 @@
+//Adding Form Inputs
+//NewExpense.js
+import React from "react";
+import "./NewExpense.css";
+
+import ExpenseForm from "./ExpenseForm";
+
+const NewExpense = () => {
+  return (
+    <div className="new-expense">
+      <ExpenseForm />
+    </div>
+  );
+};
+
+export default NewExpense;
+
+
+//Listening to User Input
+//ExpenseForm.js
+import React from "react";
+
+import "./ExpenseForm.css";
+
+const ExpenseForm = () => {
+  const titleChangeHandler = (event) => {
+    console.log(event.target.value);
+  }; //for onChange, intuitive to the values we type in the form using keyboard
+
+  return (
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text" onChange={titleChangeHandler} />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input type="number" min="0.01" step="0.01" />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input type="date" min="2019-01-01" max="2030-12-31" />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
+  
