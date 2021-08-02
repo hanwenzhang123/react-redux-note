3178b2155276a5f2e46f18e81a442bbfc4ff079d
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-08-02 14:21:14 -0400

    Update and rename 06-lifting-state.js to 06-lifting-state

--- acade-mind/02-state/06-lifting-state.js
+++
@@ -1,7 +0,0 @@
-//Lifting The State Up
-
-// This is component has access to both ivolved components
-                  |<-               <App />                 <-|
-//pass state data via props       /         \          //lifting the state up
-                  |->  <Expenses />   -X-   <NewExpense />  ->|
-//          Data/State is needed here    Data/State is generated here
