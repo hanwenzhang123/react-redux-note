f3b4f0c2ac46d8e48720a50f4b06b8f011ee3b51
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-08-02 13:35:02 -0400

    Update 05-bottom-up.js

--- acade-mind/02-state/05-bottom-up.js
+++ acade-mind/02-state/05-bottom-up.js
@@ -1,4 +1,161 @@
 //Child-to-Parent Component Communication (Botttom-up)
+//Parent-to-Child we use props
+
+//communicate up from child to parent:
+//We can create our own event props, and we can expect functionsas values 
+//and that would allow us to pass a function from a parent component to a child component 
+//and then call that function inside of the child component.
+//and when we then call a function, we can of course pass data to that function as a parameter 
+
+//ExpenseForm.js
+import React, { useState } from "react";
+
+import "./ExpenseForm.css";
+
+const ExpenseForm = (props) => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    //pass the expenseDate which generated as our argument and that is the value we received as a parameter in NewExpress
+    props.onSaveExpenseData(expenseData); //execute the onSaveExpenseDataHandler function in NewExpense
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={enteredAmount}
+            onChange={amountChangeHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            value={enteredDate}
+            onChange={dateChangeHandler}
+          />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
 
 
- 
+//NewExpense.js
+import React from "react";
+import "./NewExpense.css";
+
+import ExpenseForm from "./ExpenseForm";
+
+const NewExpense = (props) => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData, //pull out all the expenseData key-value pairs from ExpenseForm to this new object
+      id: Math.random().toString(), //new ID
+    };
+    props.onAddExpense(expenseData); //will forward the enriched expenseDate
+  };
+
+  return (
+    <div className="new-expense">
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {/* onSaveExpenseData is our component, point to the saveExpenseDataHandler as a value 
+      that passes to ExpenseForm, not execute*/}
+    </div>
+  );
+};
+
+export default NewExpense;
+
+
+//App.js
+import React from "react";
+
+import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
+
+const App = () => {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
+  const addExpenseHandler = (expense) => {
+    //pass the expense
+    console.log("In App.js");
+    console.log(expense); //log the single expense
+  };
+
+  return (
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
+    </div>
+  );
+};
+
+export default App;
+
+
