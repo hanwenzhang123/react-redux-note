31dbd2b39c2fe895c0f91fb04a29a90e2ec62c1f
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-08-03 10:56:30 -0400

    Create 02-filter.js

---
+++ acade-mind/03-rendering/02-filter.js
@@ -0,0 +1,39 @@
+//Expenses.js
+import React, { useState } from "react";
+
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import "./Expenses.css";
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.item.filter((expense) => { //filter returns a brand new array
+    return expense.date.getFullYear().toString() === filteredYear; //return true or false
+  });
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFiler={filterChangeHandler}
+        />
+        {filteredExpenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
