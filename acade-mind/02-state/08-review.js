7d28863cb9834f57e9881626ef8660e11cae76ad
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-08-03 09:20:51 -0400

    Update 08-review.js

--- acade-mind/02-state/08-review.js
+++ acade-mind/02-state/08-review.js
@@ -1,9 +1,9 @@
 1. How should you NOT listen to events when working with React?
 Adding an event listener (e.g. via "addEventListener") Manully
 because this is how you should NOT set up event listening. 
-This would be imperative code, you're not using React's features with this code 
+This would be imperative code, you are not using React is features with this code 
 and you would trigger some function that lives outside of React components 
-and hence wouldn't be able to interact with React component state.
+and hence would not be able to interact with React component state.
 
 2.Which value should you pass to event listener props like onClick?
 A pointer at the function that should execute when the event occurs. 
@@ -15,14 +15,14 @@ You can accept a function via props and call it form the inside the lower-level 
 (which passed the function)
 In JavaScript, functions are just objects (i.e. regular values) and hence you can pass them as values via props to a component. 
 If that component then calls that function, it executes 
-- and that's how you can trigger a function defined in a parent component from inside a child component.
+- and that is how you can trigger a function defined in a parent component from inside a child component.
 
 4. How can you change what a component displays on the screen?
 create some "state" value (via useState) which you can then change and output in JSX.
 
 5. Why do you need this extra "state" concept instead of regular JS variables which you change and use?
 Because standard JS variables do not cause React components to be re-evaluated
-React doesn't care whether you changed some variable values. It'll not re-evaluate the component function. 
+React does not care whether you changed some variable values. It will not re-evaluate the component function. 
 It only does that for changes to registered state values (created via useState)
 
 6. Which statement about useState is correct?
@@ -38,7 +38,7 @@ Calling that function will then also trigger React to re-evaluate the component.
 8. How much state may you manage in one single component?
 You can have as many state slices as you need/want 
 
-9. What's wrong about this code snippet?
+9. What is wrong about this code snippet?
 const [counter, setCounter] = useState(1);
 ...
 setCounter(counter + 1);
