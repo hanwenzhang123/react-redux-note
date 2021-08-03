6c2ae63b7b3b17917a3f6dfd2fa3a1785f786d77
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-08-03 10:34:00 -0400

    Update 01-map.js

--- acade-mind/03-rendering/01-map.js
+++ acade-mind/03-rendering/01-map.js
@@ -64,3 +64,52 @@ const Expenses = (props) => {
 
 export default Expenses;
   
+
+//App.js
+import React, { useState } from "react";
+
+import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
+
+const DUMMY_EXPENSES = [
+  //initial expenses
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES); //DUMMY_EXPENSES as the initial expenses data
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      //pass a function to the state updating function which automatically receives the previous expenses state
+      return [expense, ...prevExpenses]; //return new array with the new expense
+    }); //passing the new expense as the parameter to the argument
+  }; //...prevExpenses pulls out all the existing expenses
+
+  return (
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
+    </div>
+  );
+};
+
+export default App;
+  
