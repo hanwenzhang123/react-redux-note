5b75d98c66a207a5bacfbb8ddd1af7452c00de15
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-08-03 10:20:21 -0400

    Update and rename 01-basics to 01-map.js

---
+++ acade-mind/03-rendering/01-map.js
@@ -0,0 +1,66 @@
+Rendering lists & Conditional Content
+working with really dynamic content
+- how we can output arrays of data on our page 
+and how we can show different content based on different conditions
+
+//Rendering Lists of Data
+map() creates a new array based on another array and transform every element in that original array 
+map() takes a function as argument, and that function executes for every element in the array
+and it gets that element for which it is currently executing as a parameter.
+
+import React, { useState } from "react";
+
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import "./Expenses.css";
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFiler={filterChangeHandler}
+        />
+        {props.items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+        {/*map every expense in expenses array in the App.js, map every expense into an expense item JSX element. 
+        no more hard-coded array, only dynamically*/}
+        {/* <ExpenseItem
+          title={props.items[0].title}
+          amount={props.items[0].amount}
+          date={props.items[0].date}
+        />
+        <ExpenseItem
+          title={props.items[1].title}
+          amount={props.items[1].amount}
+          date={props.items[1].date}
+        />
+        <ExpenseItem
+          title={props.items[2].title}
+          amount={props.items[2].amount}
+          date={props.items[2].date}
+        />
+        <ExpenseItem
+          title={props.items[3].title}
+          amount={props.items[3].amount}
+          date={props.items[3].date}
+        /> */}
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
+  
