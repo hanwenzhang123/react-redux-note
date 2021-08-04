9d57b769542aee213c7443009c06c3e512199547
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-08-04 09:44:58 -0400

    Create 03-dynamic-class.js

---
+++ acade-mind/04-styling/03-dynamic-class.js
@@ -0,0 +1,43 @@
+//Setting CSS Classes Dynamically
+
+//CourseInput.js
+import React, { useState } from "react";
+
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
+
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
+
+  const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
+    setEnteredValue(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+    props.onAddGoal(enteredValue);
+  };
+
+  return (
+    //dynamically add or remove classes instead of just hard code the string value className="form-control invalid"
+    //using {} for JS code, template literal `` with ${} dynamic value, can be any JS expression
+    <form onSubmit={formSubmitHandler}>
+      <div className={`form-control ${!isValid ? "invalid" : ""}`}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
+      </div>
+      <Button type="submit">Add Goal</Button>
+    </form>
+  );
+};
+
+export default CourseInput;
+ 
