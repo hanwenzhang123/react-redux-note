b0a01b7038123c9fa8f30c47b29e904dddf94768
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-08-04 10:43:24 -0400

    Create 04-styled-components.js

---
+++ acade-mind/04-styling/04-styled-components.js
@@ -0,0 +1,48 @@
+Introducing Styled Components
+a package that helps you to build componnets which have certian styles attached to them 
+where the styles really only affect the components to which they were attached and not any other components
+
+npm install --save styled-components
+
+
+//Button.js
+import styled from "styled-components"; //import the package
+
+//``is called tagged template literal, a default js feature, which will be executed as a method behind the scene
+//button is a method from package, then using `` for passing the method to the button
+//styled.button``will provide a button, can be h1 p or other html tags
+//& for pseudo selectors
+
+const Button = styled.button`
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #8b005d;
+  color: white;
+  background: #8b005d;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
+
+  &:focus {
+    outline: none;
+  }
+
+  &:hover,
+  &:active {
+    background: #ac0e77;
+    border-color: #ac0e77;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
+`;
+// this button will give us the styles as above
+// the button which is returned applies all the props you might be passing to your own button component which we are exporting here in the end.
+
+// const Button = (props) => {
+//   return (
+//     <button type={props.type} className="button" onClick={props.onClick}>
+//       {props.children}
+//     </button>
+//   );
+// };
+
+export default Button;
+  
