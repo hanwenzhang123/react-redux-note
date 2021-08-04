dc09bbd1c8b8f06259e569ba6d3c180c6fb20e2e
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-08-04 08:52:54 -0400

    Create 02-inline-styling.js

---
+++ acade-mind/04-styling/02-inline-styling.js
@@ -0,0 +1,46 @@
+//CourseInput.js
+import React, { useState } from "react";
+
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
+
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
+
+  const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      //once we start typing, the isValid becomes true
+      setIsValid(true);
+    }
+    setEnteredValue(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      //you can not enter empty input, if the length is 0 then update isValid to false
+      setIsValid(false);
+      return;
+    }
+    props.onAddGoal(enteredValue);
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className="form-control">
+        {/* the label will become red color if we have not entered anything, camel case for CSS here due to JavaScript */}
+        <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
+        <input
+          style={{ borderColor: !isValid ? "salmon" : "transparent" }}
+          type="text"
+          onChange={goalInputChangeHandler}
+        />
+      </div>
+      <Button type="submit">Add Goal</Button>
+    </form>
+  );
+};
+
+export default CourseInput;
+  
