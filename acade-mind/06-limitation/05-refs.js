d1d54985fb7935d72bb788ededfd30a982adda9f
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-08-08 23:13:02 -0400

    Update 05-refs.js

--- acade-mind/06-limitation/05-refs.js
+++ acade-mind/06-limitation/05-refs.js
@@ -3,7 +3,7 @@ getting access to elements
 they allow us to get access to other DOM elements and work with them.
 with refs, we can set up a connection between a HTML element which is being rendered in the end and our other JavaScript code.
 
-//Original Purpose:
+//Original Purpose - code at below:
 //We simply have our state and with every keystroke, we update our state.
 // With every keystroke, we update the value we get
 // by the user and we store it in our state
@@ -54,7 +54,7 @@ const AddUser = (props) => {
     }
     props.onAddUser(enteredName, enteredUserAge);
     nameInputRef.current.value = '';    //reset input by resettinng the state
-    ageInputRef.current.value = '';     //reset input by resettinng the state 
+    ageInputRef.current.value = '';     //uncontrolled component
   };
 
   const errorHandler = () => {
@@ -73,9 +73,9 @@ const AddUser = (props) => {
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
-          <input id="username" type="text" ref={nameInputRef} />  //add ref, fetch input data
+          <input id="username" type="text" ref={nameInputRef} />  //add ref, fetch input data, inputs uncontrolled
           <label htmlFor="age">Age (Years)</label>
-          <input id="age" type="number" ref={ageInputRef} />    //connect HTML to reference
+          <input id="age" type="number" ref={ageInputRef} />    //connect HTML to reference, internal state
           <Button type="submit">Add User</Button>
         </form>
       </Card>
@@ -84,4 +84,99 @@ const AddUser = (props) => {
 };
 
 export default AddUser;
- 
+
+
+//Uncontrolled Component
+This approach of using refs to interact with dom elements specifically with input elements also has a special name.
+We are talking about uncontrolled components if we access values with a ref.
+
+The input above are internal state, so to value which is reflected in them is not controlled by React.
+we are not controlling these state off the input element with react.
+
+//Controlled Component
+The approach we had before, where we manage our state and we updated that state on every keystroke
+and we feed that state back into the input with the value prop
+those input fields are controlled components because their internal state is controlled by react.
+  
+import React, { useState } from 'react';
+
+import Card from '../UI/Card';
+import Button from '../UI/Button';
+import ErrorModal from '../UI/ErrorModal';
+import Wrapper from '../Helpers/Wrapper';
+import classes from './AddUser.module.css';
+
+const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
+  const [error, setError] = useState();
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: 'Invalid input',
+        message: 'Please enter a valid name and age (non-empty values).',
+      });
+      return;
+    }
+    if (+enteredAge < 1) {
+      setError({
+        title: 'Invalid age',
+        message: 'Please enter a valid age (> 0).',
+      });
+      return;
+    }
+    props.onAddUser(enteredUsername, enteredAge);
+    setEnteredUsername('');
+    setEnteredAge('');
+  };
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
+  const errorHandler = () => {
+    setError(null);
+  };
+
+  return (
+    <Wrapper>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            value={enteredUsername}
+            onChange={usernameChangeHandler}
+          />
+          <label htmlFor="age">Age (Years)</label>
+          <input
+            id="age"
+            type="number"
+            value={enteredAge}
+            onChange={ageChangeHandler}
+          />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </Wrapper>
+  );
+};
+
+export default AddUser;
+
+  
+  
