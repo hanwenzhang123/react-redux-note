5025053e606201650878a863be7bf8408b36ab2a
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-08-09 23:34:49 -0400

    Update 05-useEffect-cleanup.js

--- acade-mind/07-effect/05-useEffect-cleanup.js
+++ acade-mind/07-effect/05-useEffect-cleanup.js
@@ -1,1 +1,104 @@
+So the cleanup function runs before every new side effect function execution and before the component is removed.
+And it does not run before the first side effect function execution.
+But thereafter, it will run before every next side effect function execution.
 
+//cleanup function
+cleanup function will run as a cleanup process before useEffect executes this function the next time.
+If we want to be sending an HTTP request, we would have now only sent once instead of a dozen HTTP requests.
+
+//Process
+using setTimeOut(), then return the clearTimeOut() to pass the function expression setTimeOut
+one ongoing timer at the a time, as long as the user keeps on typing, we clear all other timers
+
+
+//Login.js
+import React, { useState, useEffect } from "react";
+
+import Card from "../UI/Card/Card";
+import classes from "./Login.module.css";
+import Button from "../UI/Button/Button";
+
+const Login = (props) => {
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [emailIsValid, setEmailIsValid] = useState();
+  const [enteredPassword, setEnteredPassword] = useState("");
+  const [passwordIsValid, setPasswordIsValid] = useState();
+  const [formIsValid, setFormIsValid] = useState(false);
+
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      //we save the timer, and for the next keystroke we clear it
+      setFormIsValid(
+        enteredEmail.includes("@") && enteredPassword.trim().length > 6
+      );
+    }, 500); //wait 500 milliseconds to check the form validation
+
+    return () => { //return has to be function itself, it is a clean up function
+      clearTimeout(identifier); //we clear the last timer before we set a new one.
+      // the code above setFormIsValid only ran once for all those keystrokes.
+    }; 
+  }, [enteredEmail, enteredPassword]);
+
+  const emailChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+
+  const passwordChangeHandler = (event) => {
+    setEnteredPassword(event.target.value);
+  };
+
+  const validateEmailHandler = () => {
+    setEmailIsValid(enteredEmail.includes("@"));
+  };
+
+  const validatePasswordHandler = () => {
+    setPasswordIsValid(enteredPassword.trim().length > 6);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    props.onLogin(enteredEmail, enteredPassword);
+  };
+
+  return (
+    <Card className={classes.login}>
+      <form onSubmit={submitHandler}>
+        <div
+          className={`${classes.control} ${
+            emailIsValid === false ? classes.invalid : ""
+          }`}
+        >
+          <label htmlFor="email">E-Mail</label>
+          <input
+            type="email"
+            id="email"
+            value={enteredEmail}
+            onChange={emailChangeHandler}
+            onBlur={validateEmailHandler}
+          />
+        </div>
+        <div
+          className={`${classes.control} ${
+            passwordIsValid === false ? classes.invalid : ""
+          }`}
+        >
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={enteredPassword}
+            onChange={passwordChangeHandler}
+            onBlur={validatePasswordHandler}
+          />
+        </div>
+        <div className={classes.actions}>
+          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+            Login
+          </Button>
+        </div>
+      </form>
+    </Card>
+  );
+};
+
+export default Login;
