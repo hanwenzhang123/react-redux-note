419e620099bdb428e77c710d5a922103c17c29e3
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-08-22 22:12:52 -0400

    Create 01-basics.js

---
+++ acade-mind/09-classes/01-basics.js
@@ -0,0 +1,22 @@
+//Class-based Components: An alternative to functions
+optional, react past way of building components
+an alternative way of building components
+- what & why
+- working with class-based components
+- error boundaries (require class-based components)
+
+//What & Why
+//Functional Components (default, most modern approach)
+function Product(props) {   //usually using const with arrow function
+  return <h2> product!</h2>
+}
+//components are regular javascript function which return renderable results (typically JSX)
+
+//Class-based Components (was required in the past prior to react 16.8)
+class Product extends Component {
+  render(){   //reserved word
+    return <h2> product!</h2>
+  }
+}
+//components can also be defined as JS classes where a render() method defines the to-be-rendered output
+ 
