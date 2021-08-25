10fbd82340767a78e2f2e7758f38d89f28d1007f
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-08-24 21:50:51 -0400

    Update 01-card.js

--- zero-to-mastery/01-card.js
+++ zero-to-mastery/01-card.js
@@ -1,3 +1,70 @@
+//index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
+//App.js 
+import React, { Component } from 'react';
+
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
+  onSearchChange = event => {
+    this.setState({ searchField: event.target.value });
+  };
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
+    return (
+      <div className='App'>
+        <h1>Monsters Rolodex</h1>
+        <SearchBox onSearchChange={this.onSearchChange} />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+//App.test.js 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
 //card-list.component.jsx 
 import React from 'react';
 import { Card } from '../card/card.component';
@@ -11,6 +78,8 @@ export const CardList = props => (
   </div>
 );
 
+
+//components
 //card.component.jsx
 import React from 'react';
 import './card.styles.css';
@@ -26,3 +95,14 @@ export const Card = props => (
   </div>
 );
   
+//search-box.component.jsx
+import React from 'react';
+import './search-box.styles.css';
+
+export const SearchBox = props => (
+  <input
+    className='search-box'
+    type='search'
+    placeholder='search monsters'
+    onChange={props.onSearchChange}
+  />
