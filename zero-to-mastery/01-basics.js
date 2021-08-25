931b0191e4ddaf3755053a9faa259e3a532fb709
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-08-24 22:04:25 -0400

    Create 01-basics.js

---
+++ zero-to-mastery/01-basics.js
@@ -0,0 +1,11 @@
+//breaking into components
+state is always something that changes because user interaction
+state usually lives in one location or a specific state only one location, it trickles down as props
+one way data flow, data pass down through props
+
+if a state changes, it is going to happen through setState
+it will anounce passing down to re-render
+each component is a function that renders
+if state changes, it is going to notify its component run the render function again
+
+ 
