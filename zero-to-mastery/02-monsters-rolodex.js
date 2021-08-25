cf59e7ba154a717260b9ac5d78a7e99c346dc3c0
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-08-24 23:10:14 -0400

    Update 02-monsters-rolodex.js

--- zero-to-mastery/02-monsters-rolodex.js
+++ zero-to-mastery/02-monsters-rolodex.js
@@ -15,28 +15,28 @@ import './App.css';
 
 class App extends Component {
   constructor() {
-    super();
+    super();    //helps us with this by calling react.component's constructor()
 
-    this.state = {
-      monsters: [],
+    this.state = {  //dynamic content
+      monsters: [],   //initially state empty
       searchField: ''
     };
   }
 
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({ monsters: users }));
+  componentDidMount() {   //puts our components on the page, it renders onto the DOM for the first time.
+    fetch('https://jsonplaceholder.typicode.com/users')   //fetching content
+      .then(response => response.json())    //get the actual body in the json format
+      .then(users => this.setState({ monsters: users })); //setState to set data
   }
 
-  onSearchChange = event => {
-    this.setState({ searchField: event.target.value });
+  onSearchChange = event => {   //arrow function allows you to set this when it defines in first place, so we do not need to bind as the regular function
+    this.setState({ searchField: event.target.value });   //setState is not happening immediately because it is async call
   };
 
   render() {
-    const { monsters, searchField } = this.state;
-    const filteredMonsters = monsters.filter(monster =>
-      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    const { monsters, searchField } = this.state;   //destructuring from this.state, pulling out the value to the constance
+    const filteredMonsters = monsters.filter(monster =>   //get back a new array after filtering
+      monster.name.toLowerCase().includes(searchField.toLowerCase())    //includes returns true or false
     );
 
     return (
@@ -52,31 +52,19 @@ class App extends Component {
 export default App;
 
 
-//App.test.js 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
-
-
 //components
 //card-list.component.jsx 
 import React from 'react';
 import { Card } from '../card/card.component';
 import './card-list.styles.css';
 
-export const CardList = props => (
+export const CardList = props => (    //props that have passed down from parents
   <div className='card-list'>
-    {props.monsters.map(monster => (
+    {props.monsters.map(monster => (    //we decide how the card looks like in the Card component
       <Card key={monster.id} monster={monster} />
     ))}
-  </div>
-);
+  </div> 
+);    //{prop.children} - Children are actually what you pass in between the brackets of our component that gets called.
 
 
 //card.component.jsx
@@ -92,14 +80,14 @@ export const Card = props => (
     <h2> {props.monster.name} </h2>
     <p> {props.monster.email} </p>
   </div>
-);
+);      // we pass individual monster to the card component
   
   
-//search-box.component.jsx
+//search-box.component.jsx      //the search box component, make it reusable
 import React from 'react';
 import './search-box.styles.css';
 
-export const SearchBox = props => (
+export const SearchBox = props => (   //props for the data passing down from App
   <input
     className='search-box'
     type='search'
