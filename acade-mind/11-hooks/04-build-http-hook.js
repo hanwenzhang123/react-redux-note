150bd7f5271aa7ed08b47f4be43160e8f8a5e703
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-08-30 16:05:27 -0400

    Create 04-build-http-hook.js

---
+++ acade-mind/11-hooks/04-build-http-hook.js
@@ -0,0 +1,41 @@
+//Building a custom HTTP hooks
+a custom hook is more generic so we can pass down to use it with components
+
+//hooks/use-http.js
+import { useState } from 'react';
+
+const useHttp = (requestConfig, applyData) => {   //must start with use, pass in params requestConfig object and applyData function
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = async () => {     //send request with a more generic name
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(requestConfig.url, {   //the requestConfig is passed in with dynamic url 
+        method: requestConfig.method,
+        headers: requestConfig.headers,
+        body: JSON.stringify(requestConfig.body),   //json transformation
+      });
+
+      if (!response.ok) {
+        throw new Error('Request failed!');
+      }
+
+      const data = await response.json();
+      applyData(data);    //here we pass the data converted through json to the function applyData that passed as an argument
+    } catch (err) {
+      setError(err.message || 'Something went wrong!');
+    }
+    setIsLoading(false);
+  };
+
+  return {    //return where the custom hoook are used, here we return an object
+    isLoading,      //isLoading: isLoading (same property names so we can use new JS shortcut syntax)
+    error,          //error: error
+    sendRequest,    //sendRequest: sendRequest
+  };
+};
+
+export default useHttp;
+  
