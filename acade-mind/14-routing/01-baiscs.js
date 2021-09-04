b31cbb14a479b020bcdf6d8bea24c9ace46605eb
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-09-04 10:03:07 -0400

    Create 01-baiscs.js

---
+++ acade-mind/14-routing/01-baiscs.js
@@ -0,0 +1,7 @@
+Single Page Application Routing
+Multiple pages in single page app
+- what is client side routing
+- using react router
+- advanced features: dynamic and nested routes
+
+//What is routing? why?
