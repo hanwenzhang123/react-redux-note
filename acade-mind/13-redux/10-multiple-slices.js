e936659042197c83037adb75a809d02556b8fd6e
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-09-04 16:18:26 -0400

    Update 10-multiple-slices.js

--- acade-mind/13-redux/10-multiple-slices.js
+++ acade-mind/13-redux/10-multiple-slices.js
@@ -50,3 +50,120 @@ export const authActions = authSlice.actions;   //expose actions
 
 export default store;
 
+
+//components/Header.js
+import { useSelector, useDispatch } from 'react-redux';
+
+import classes from './Header.module.css';
+import { authActions } from '../store/index';
+
+const Header = () => {
+  const dispatch = useDispatch();
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+
+  const logoutHandler = () => {
+    dispatch(authActions.logout());
+  };
+
+  return (
+    <header className={classes.header}>
+      <h1>Redux Auth</h1>
+      {isAuth && (
+        <nav>
+          <ul>
+            <li>
+              <a href='/'>My Products</a>
+            </li>
+            <li>
+              <a href='/'>My Sales</a>
+            </li>
+            <li>
+              <button onClick={logoutHandler}>Logout</button>
+            </li>
+          </ul>
+        </nav>
+      )}
+    </header>
+  );
+};
+
+export default Header;
+
+
+//components/Auth.js
+import { useDispatch } from 'react-redux';
+
+import classes from './Auth.module.css';
+import { authActions } from '../store/index';
+
+const Auth = () => {
+  const dispatch = useDispatch();
+
+  const loginHandler = (event) => {
+    event.preventDefault();
+
+    dispatch(authActions.login());
+  };
+
+  return (
+    <main className={classes.auth}>
+      <section>
+        <form onSubmit={loginHandler}>
+          <div className={classes.control}>
+            <label htmlFor='email'>Email</label>
+            <input type='email' id='email' />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor='password'>Password</label>
+            <input type='password' id='password' />
+          </div>
+          <button>Login</button>
+        </form>
+      </section>
+    </main>
+  );
+};
+
+export default Auth;
+
+
+//components/UserProfile.js
+import classes from './UserProfile.module.css';
+
+const UserProfile = () => {
+  return (
+    <main className={classes.profile}>
+      <h2>My User Profile</h2>
+    </main>
+  );
+};
+
+export default UserProfile;
+
+
+//App.js
+import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
+
+import Counter from './components/Counter';
+import Header from './components/Header';
+import Auth from './components/Auth';
+import UserProfile from './components/UserProfile';
+
+
+function App() {
+  const isAuth = useSelector(state => state.auth.isAuthenticated);
+
+  return (
+    <Fragment>
+      <Header />
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />}
+      <Counter />
+    </Fragment>
+  );
+}
+
+export default App;
+
+ 
