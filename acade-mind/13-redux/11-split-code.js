f01241c7b7553e05b01907901e10330ff8f27e62
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-09-04 16:12:59 -0400

    Update 11-split-code.js

--- acade-mind/13-redux/11-split-code.js
+++ acade-mind/13-redux/11-split-code.js
@@ -1,1 +1,67 @@
+//store/index.js
+import { configureStore } from '@reduxjs/toolkit';
 
+import counterReducer from './counter';
+import authReducer from './auth';
+
+
+const store = configureStore({
+  reducer: { counter: counterReducer, auth: authReducer },
+});
+
+export default store;
+
+
+//store/counter.js
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialCounterState = { counter: 0, showCounter: true };
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: initialCounterState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
+export const counterActions = counterSlice.actions;
+
+export default counterSlice.reducer;
+
+
+//store/auth.js
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialAuthState = {
+  isAuthenticated: false,
+};
+
+const authSlice = createSlice({
+  name: 'authentication',
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
+export const authActions = authSlice.actions;
+
+export default authSlice.reducer;
+ 
