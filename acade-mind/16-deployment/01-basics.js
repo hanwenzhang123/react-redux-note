58a23dc8b3778e312845f8b9a2c587182b68e53c
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-09-04 10:18:02 -0400

    Update 01-basics.js

--- acade-mind/16-deployment/01-basics.js
+++ acade-mind/16-deployment/01-basics.js
@@ -2,3 +2,10 @@ Deploying React App
 From development to production
 - Deployment steps and pitfalls
 - server-side routing vs client-side routing
+
+Deployment Steps
+- Test code
+- Optimize code
+- Build app for production
+- Upload production code to server
+- Configure server
