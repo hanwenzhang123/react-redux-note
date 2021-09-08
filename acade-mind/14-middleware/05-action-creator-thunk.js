3dc4d8b15437a0ac97a8266ca8d956bd7d28cffd
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-09-08 00:35:09 -0400

    Update 05-action-creator-thunk.js

--- acade-mind/14-middleware/05-action-creator-thunk.js
+++ acade-mind/14-middleware/05-action-creator-thunk.js
@@ -98,3 +98,47 @@ export const sendCartData = (cart) => {   //add Action Creator here
 export const cartActions = cartSlice.actions;
 
 export default cartSlice;
+
+//App.js
+import { Fragment, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
+import Notification from './components/UI/Notification';
+import { sendCartData } from './store/cart-slice';    //import it
+
+let isInitial = true;
+
+function App() {
+  const dispatch = useDispatch();
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    dispatch(sendCartData(cart));   //call the sendCartData() and passing "cart" to it
+  }, [cart, dispatch]);
+
+  return (
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
+  );
+}
