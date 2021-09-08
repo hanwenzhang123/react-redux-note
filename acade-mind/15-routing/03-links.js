8cbc543304b00aad56fad4699a374b78db18040b
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-09-08 15:06:58 -0400

    Update 03-links.js

--- acade-mind/15-routing/03-links.js
+++ acade-mind/15-routing/03-links.js
@@ -25,7 +25,36 @@ const MainHeader = () => {
 
 export default MainHeader;
 
+
 //<NavLink to="/{actual address we want to link to}"> </NavLink>
 Basically like the standard link.
 It still creates an anchored tag catches the click, prevents the browser default
 NavLink also will set a CSS class on the active anchor item.
+
+//components/MainHeader.js
+import { NavLink } from 'react-router-dom';
+
+import classes from './MainHeader.module.css';
+
+const MainHeader = () => {
+  return (
+    <header className={classes.header}>
+      <nav>
+        <ul>
+          <li>
+            <NavLink activeClassName={classes.active} to='/welcome'>    //change to NavLink and add activeClassName prop if link is active
+              Welcome
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName={classes.active} to='/products'>   //change to NavLink and add activeClassName prop if link is active
+              Products
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainHeader;
