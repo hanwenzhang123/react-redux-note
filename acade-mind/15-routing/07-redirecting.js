5a04971499cebe74e1c6fcdb1fb59c79a58b2aa4
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-09-08 16:01:31 -0400

    Update 07-redirecting.js

--- acade-mind/15-routing/07-redirecting.js
+++ acade-mind/15-routing/07-redirecting.js
@@ -1,1 +1,38 @@
+//Redirecting The User
+//App.js
+import { Route, Switch, Redirect } from 'react-router-dom';   //import the Redirect
 
+import Welcome from './pages/Welcome';
+import Products from './pages/Products';
+import ProductDetail from './pages/ProductDetail';
+import MainHeader from './components/MainHeader';
+
+function App() {
+  return (
+    <div>
+      <MainHeader />
+      <main>
+        <Switch>
+          <Route path='/' exact>    //like home page "/", exact matters
+            <Redirect to='/welcome' />    //redirect the user somewhere else, here redirect to the welcome page
+          </Route>
+          <Route path='/welcome'>
+            <Welcome />
+          </Route>
+          <Route path='/products' exact>
+            <Products />
+          </Route>
+          <Route path='/products/:productId'>
+            <ProductDetail />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
+}
+
+export default App;
+
+// our-domain.com/welcome => Welcome Component
+// our-domain.com/products => Products Component
+// our-domain.com/product-detail/a-book
