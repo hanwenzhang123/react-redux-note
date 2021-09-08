31861436c33ba2979c4da92cfecce3f9e85ac1ef
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-09-08 12:39:08 -0400

    Create 02-setup.js

---
+++ acade-mind/15-routing/02-setup.js
@@ -0,0 +1,39 @@
+//Using Routes
+//index.js
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";   //import
+
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(    //wrap the App
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+
+//App.js
+import { Route } from "react-router-dom";   //import
+
+import Welcome from "./components/Welcome";
+import Products from "./components/Products";
+
+function App() {
+  return (      //using <Route path="/{path}"> </Route> to wrap the component you would like to run with that URL
+    <div>
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+      <Route path="/products">
+        <Products />
+      </Route>
+    </div>
+  );
+}
+
+export default App;
+
+// our-domain.com/welcome => Welcome Component
+// our-domain.com/products => Products Component
+ 
