2d7a8e9c6eb157a236c86bf19664181e3bee10eb
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-09-08 11:53:23 -0400

    Update 01-baiscs.js

--- acade-mind/15-routing/01-baiscs.js
+++ acade-mind/15-routing/01-baiscs.js
@@ -4,4 +4,18 @@ Multiple pages in single page app
 - using react router
 - advanced features: dynamic and nested routes
 
-//What is routing? why?
+//What is Routing? Why?
+URL changes, visible content changes
+//Traditional Multi-page Routing
+HTML is requested & loaded
+Page change = new request + response
+//Building Single Page application
+when building complex user interfaces, we typically build Single Page Application (SPAs)
+Only one initial HTML request & response
+Page (URL) changes are then handled by client-side (React) code -> changes the visible content without fetching a new HTML file
+
+// React Router 
+  - npm install react-router-dom
+https://reactrouter.com/
+https://github.com/remix-run/react-router
+ 
