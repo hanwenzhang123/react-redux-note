fd6b7a781b87f6109f8c505190532cfd9e9d6b60
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-09-08 22:32:05 -0400

    Update 01-basics.js

--- acade-mind/16-deployment/01-basics.js
+++ acade-mind/16-deployment/01-basics.js
@@ -13,6 +13,7 @@ Deployment Steps
 Optimization
 - useMemo()
 
+//Deployment
 //Build the code for production
 npm run build 
 - build production output
@@ -22,8 +23,27 @@ npm run build
 //Getting Started with Deployment (uploading files)
 A react SPA is a "Static Website"
 - only HTML, CSS & JavaScript
+- no server side involved
+- a static site host is needed
+
+//Using Firebase for deployment
+sudo npm install -g firebase-tools    //sugo gives the right permission, -g install globally
+follow the steps on set up firebase hoisting
 
 //Exploring Routing Issues & Finishing Deployment
+server-side routing vs client-side routing
+serve - production-ready react code
+client - user
 
+//Different URLs trigger different actions and will lead to different responses.
+user sends request to the server, then server sends back response with html css js react code
+then react contains react router code that will bring the correct component onto the screen based on the part of the URL after evaluation
+Actutally the URL which the user entered with the specific path entered by the user is also part of the requests already though.
+So the request which reaches the server contains this path.
+a server would then look for different files which it would return as a response for different URLs.
 
+//Hosting Single Page Application - React
+when hosting a Single Page Application, we wanna ignore the path, and always return the same response,
+the same HTML file and the same JavaScript files no matter which path the user targeted.
+React Router, which is part of that application, which will take another look at that URL and then render the correct content on the screen.
   
