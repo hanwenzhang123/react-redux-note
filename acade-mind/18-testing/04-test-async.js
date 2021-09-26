68359de565af7c23806625fcc7f79d5dcc811003
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-09-25 23:34:50 -0400

    Update 04-test-async.js

--- acade-mind/18-testing/04-test-async.js
+++ acade-mind/18-testing/04-test-async.js
@@ -1,4 +1,4 @@
-//Async.test.js
+//Async.test.js - Testing Asynchronous Code
 import { render, screen } from '@testing-library/react';
 import Async from './Async';
 
@@ -10,3 +10,30 @@ describe('Async component', () => {
     expect(listItemElements).not.toHaveLength(0);
   });
 });
+
+//Async.js
+import { useEffect, useState } from 'react';
+
+const Async = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((data) => {
+        setPosts(data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Async;
