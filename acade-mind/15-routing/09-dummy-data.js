d952cdd23d572cd824c66ae9f04228494cc939ee
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-10-08 13:47:28 -0400

    Update 09-dummy-data.js

--- acade-mind/15-routing/09-dummy-data.js
+++ acade-mind/15-routing/09-dummy-data.js
@@ -6,8 +6,57 @@ const DUMMY_QUOTES = [
   { id: 'q2', author: 'Maximilian', text: 'Learning React is great!' },
 ];
 
-const AllQuotes = () => {
+const AllQuotes = () => {     //passing down the dummy data to the QuoteList component
   return <QuoteList quotes={DUMMY_QUOTES} />
 };
 
 export default AllQuotes;
+
+//src/components/quotes/QuoteList.js
+import { Fragment } from 'react';
+
+import QuoteItem from './QuoteItem';
+import classes from './QuoteList.module.css';
+
+const QuoteList = (props) => {
+  return (
+    <Fragment>
+      <ul className={classes.list}>
+        {props.quotes.map((quote) => (    //map each quote to the QuoteItem component with unique key
+          <QuoteItem
+            key={quote.id}
+            id={quote.id}
+            author={quote.author}
+            text={quote.text}
+          />
+        ))}
+      </ul>
+    </Fragment>
+  );
+};
+
+export default QuoteList;
+
+
+//src/components/quotes/QuoteItem.js
+import classes from './QuoteItem.module.css';
+
+const QuoteItem = (props) => {    //display each quote
+  return (
+    <li className={classes.item}>
+      <figure>
+        <blockquote>
+          <p>{props.text}</p>
+        </blockquote>
+        <figcaption>{props.author}</figcaption>
+      </figure>
+      <a className='btn'>
+        View Fullscreen
+      </a>
+    </li>
+  );
+};
+
+export default QuoteItem;
+
+  
