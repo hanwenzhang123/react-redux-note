ab5ac604e135247c2837b63ab53a15c3e2130db0
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-10-09 13:17:49 -0400

    Update 16-send-get-data-via-http.js

--- acade-mind/15-routing/16-send-get-data-via-http.js
+++ acade-mind/15-routing/16-send-get-data-via-http.js
@@ -1,1 +1,292 @@
+//Sending & Getting Quote Data via HTTP
 
+//src/pages/NewQuote.js
+import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
+import QuoteForm from '../components/quotes/QuoteForm';
+import useHttp from '../hooks/use-http';    //import from the hook
+import { addQuote } from '../lib/api';
+
+const NewQuote = () => {
+  const { sendRequest, status } = useHttp(addQuote);
+  const history = useHistory();
+
+  useEffect(() => {   //trigger when the status and history changes
+    if (status === 'completed') {
+      history.push('/quotes');
+    }
+  }, [status, history]);
+
+  const addQuoteHandler = (quoteData) => {
+    sendRequest(quoteData);     //sending the data
+  };
+
+  return <QuoteForm isLoading={status === 'pending'} onAddQuote={addQuoteHandler} />;
+};      //isLoading runs the spinner when the status is pending
+
+export default NewQuote;
+
+
+//src/pages/AllQuotes.js
+import { useEffect } from 'react';
+
+import QuoteList from '../components/quotes/QuoteList';
+import LoadingSpinner from '../components/UI/LoadingSpinner';
+import NoQuotesFound from '../components/quotes/NoQuotesFound';
+import useHttp from '../hooks/use-http';
+import { getAllQuotes } from '../lib/api';
+
+const AllQuotes = () => {
+  const { sendRequest, status, data: loadedQuotes, error } = useHttp(
+    getAllQuotes,
+    true
+  );
+
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);    //dependency
+
+  if (status === 'pending') {
+    return (
+      <div className='centered'>
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className='centered focused'>{error}</p>;
+  }
+
+  if (status === 'completed' && (!loadedQuotes || loadedQuotes.length === 0)) {
+    return <NoQuotesFound />;
+  }
+
+  return <QuoteList quotes={loadedQuotes} />;   //pass all the test then pass all loaded quote list
+};
+
+export default AllQuotes;
+
+
+//src/pages/QuoteDetail.js
+import { Fragment, useEffect } from 'react';
+import { useParams, Route, Link, useRouteMatch } from 'react-router-dom';
+
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+import Comments from '../components/comments/Comments';
+import useHttp from '../hooks/use-http';    //import
+import { getSingleQuote } from '../lib/api';   //import
+import LoadingSpinner from '../components/UI/LoadingSpinner';  //import
+
+const QuoteDetail = () => {
+  const match = useRouteMatch();
+  const params = useParams();
+
+  const { quoteId } = params;
+
+  const { sendRequest, status, data: loadedQuote, error } = useHttp(
+    getSingleQuote,
+    true
+  );
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  if (status === 'pending') {   //check different stauts here
+    return (
+      <div className='centered'>
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className='centered'>{error}</p>;
+  }
+
+  if (!loadedQuote.text) {
+    return <p>No quote found!</p>;
+  }
+
+  return (
+    <Fragment>
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
+      <Route path={match.path} exact>
+        <div className='centered'>
+          <Link className='btn--flat' to={`${match.url}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+      <Route path={`${match.path}/comments`}>
+        <Comments />
+      </Route>
+    </Fragment>
+  );
+};
+
+export default QuoteDetail;
+
+
+//src/hooks/use-http.js
+import { useReducer, useCallback } from 'react';
+
+function httpReducer(state, action) {
+  if (action.type === 'SEND') {
+    return {
+      data: null,
+      error: null,
+      status: 'pending',
+    };
+  }
+
+  if (action.type === 'SUCCESS') {
+    return {
+      data: action.responseData,
+      error: null,
+      status: 'completed',
+    };
+  }
+
+  if (action.type === 'ERROR') {
+    return {
+      data: null,
+      error: action.errorMessage,
+      status: 'completed',
+    };
+  }
+
+  return state;
+}
+
+function useHttp(requestFunction, startWithPending = false) {
+  const [httpState, dispatch] = useReducer(httpReducer, {
+    status: startWithPending ? 'pending' : null,
+    data: null,
+    error: null,
+  });
+
+  const sendRequest = useCallback(
+    async function (requestData) {
+      dispatch({ type: 'SEND' });
+      try {
+        const responseData = await requestFunction(requestData);
+        dispatch({ type: 'SUCCESS', responseData });
+      } catch (error) {
+        dispatch({
+          type: 'ERROR',
+          errorMessage: error.message || 'Something went wrong!',
+        });
+      }
+    },
+    [requestFunction]
+  );
+
+  return {
+    sendRequest,
+    ...httpState,
+  };
+}
+
+export default useHttp;
+
+
+//src/lib/api.js
+const FIREBASE_DOMAIN = 'https://react-prep-default-rtdb.firebaseio.com';   //enter firebase URL
+
+export async function getAllQuotes() {
+  const response = await fetch(`${FIREBASE_DOMAIN}/quotes.json`);
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || 'Could not fetch quotes.');
+  }
+
+  const transformedQuotes = [];
+
+  for (const key in data) {
+    const quoteObj = {
+      id: key,
+      ...data[key],
+    };
+
+    transformedQuotes.push(quoteObj);
+  }
+
+  return transformedQuotes;
+}
+
+export async function getSingleQuote(quoteId) {
+  const response = await fetch(`${FIREBASE_DOMAIN}/quotes/${quoteId}.json`);
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || 'Could not fetch quote.');
+  }
+
+  const loadedQuote = {
+    id: quoteId,
+    ...data,
+  };
+
+  return loadedQuote;
+}
+
+export async function addQuote(quoteData) {   //send and store data in firebase
+  const response = await fetch(`${FIREBASE_DOMAIN}/quotes.json`, {
+    method: 'POST',
+    body: JSON.stringify(quoteData),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || 'Could not create quote.');
+  }
+
+  return null;
+}
+
+export async function addComment(commentData, quoteId) {
+  const response = await fetch(`${FIREBASE_DOMAIN}/comments/${quoteId}.json`, {
+    method: 'POST',
+    body: JSON.stringify(commentData),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || 'Could not add comment.');
+  }
+
+  return { commentId: data.name };
+}
+
+export async function getAllComments(quoteId) {
+  const response = await fetch(`${FIREBASE_DOMAIN}/comments/${quoteId}.json`);
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || 'Could not get comments.');
+  }
+
+  const transformedComments = [];
+
+  for (const key in data) {
+    const commentObj = {
+      id: key,
+      ...data[key],
+    };
+
+    transformedComments.push(commentObj);
+  }
+
+  return transformedComments;
+}
