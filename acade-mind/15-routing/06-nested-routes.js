f41fe617733a1cdd4eaa4dd44786f1bbcfe4e33c
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-10-09 10:57:13 -0400

    Update 06-nested-routes.js

--- acade-mind/15-routing/06-nested-routes.js
+++ acade-mind/15-routing/06-nested-routes.js
@@ -35,10 +35,52 @@ const QuoteDetail = () => {
       // this route path is constructed dynamically and it depends on the quote id for which this quote detail component was loaded.
       <Route path={`/quotes/${params.quoteId}/comments`}> 
         <Comments />    //the <Comments /> only shows when we are on "/quotes/${params.quoteId}/comments" for URL
-      </Route>   
-      //alternatively, since we are defining a route here (not a link), you can also set "path='/quotes/:quoteId/comments'"
+      </Route>    //alternatively, since we are defining a route here (not a link), you can also set "path='/quotes/:quoteId/comments'"
     </Fragment>
   );
 };
 
 export default QuoteDetail;
+
+
+//Getting Creative with Nested Routes
+//src/pages/QuoteDetail.js
+import { Fragment } from 'react';
+import { useParams, Route, Link } from 'react-router-dom';   //import Link, useRouteMatch 
+
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+import Comments from '../components/comments/Comments';
+
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'Max', text: 'Learning React is fun!' },
+  { id: 'q2', author: 'Maximilian', text: 'Learning React is great!' },
+];
+
+const QuoteDetail = () => {
+  const params = useParams();
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
+
+  return (
+    <Fragment>
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}`} exact>   //this route active if the path matches exactly, without the /comments
+        <div className='centered'>
+          <Link className='btn--flat' to=`/quotes/${params.quoteId}/comments`}> 
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+      <Route path={`/quotes/${params.quoteId}/comments`}> 
+        <Comments />
+      </Route>
+    </Fragment>
+  );
+};
+
+export default QuoteDetail;
+
