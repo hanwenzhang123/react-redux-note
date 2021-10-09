d37b53c0f652044db378437e9e1222303eb7e42f
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-10-09 12:12:49 -0400

    Create 15-flexible-routing.js

---
+++ acade-mind/15-routing/15-flexible-routing.js
@@ -0,0 +1,103 @@
+//Nested Route
+//Flexible Routing Code
+useRouteMatch - more information about currently route, a bit like useLocation
+with useRouteMatch, if we change our route like /quotes to /quote, no needs to adjust
+
+//src/pages/QuoteDetail.js
+import { Fragment } from 'react';
+import { useParams, Route, Link, useRouteMatch } from 'react-router-dom';
+
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+import Comments from '../components/comments/Comments';
+
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'Max', text: 'Learning React is fun!' },
+  { id: 'q2', author: 'Maximilian', text: 'Learning React is great!' },
+];
+
+const QuoteDetail = () => {
+  const match = useRouteMatch();  //use useRouteMatch(), we get the path that was defined by us: "/quotes/:quoteId"
+  const params = useParams();
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
+
+  return (      //match.path is the placeholder "/quotes/:quoteId"
+    <Fragment>
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={match.path} exact>
+        <div className='centered'>
+          <Link className='btn--flat' to={`${match.url}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+      <Route path={`${match.path}/comments`}>
+        <Comments />
+      </Route>
+    </Fragment>
+  );
+};
+
+export default QuoteDetail;
+
+  
+//src/components/quotes/QuoteList.js
+import { Fragment } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+
+import QuoteItem from './QuoteItem';
+import classes from './QuoteList.module.css';
+
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
+
+const QuoteList = (props) => {
+  const history = useHistory();
+  const location = useLocation();
+
+  const queryParams = new URLSearchParams(location.search);
+
+  const isSortingAscending = queryParams.get('sort') === 'asc';
+
+  const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
+
+  const changeSortingHandler = () => {    //change here to dynamics
+    history.push({      //pass an object that describe the action
+      pathname: location.pathname,      //pathname where you want to navigate to - here stays on the current page
+      search: `?sort=${(isSortingAscending ? 'desc' : 'asc')}`  //a search key allows you to add query parameter
+    });   //location has a pathname property that holds a whole current path
+  };    //(`${location.pathname}?sort=${(isSortingAscending ? `desc` : `asc`)}`)
+        //original we did hard coded('/quotes?sort=' + (isSortingAscending ? 'desc' : 'asc'))
+  return (
+    <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changeSortingHandler}>
+          Sort {isSortingAscending ? 'Descending' : 'Ascending'}
+        </button>
+      </div>
+      <ul className={classes.list}>
+        {sortedQuotes.map((quote) => (
+          <QuoteItem
+            key={quote.id}
+            id={quote.id}
+            author={quote.author}
+            text={quote.text}
+          />
+        ))}
+      </ul>
+    </Fragment>
+  );
+};
+
+export default QuoteList;
