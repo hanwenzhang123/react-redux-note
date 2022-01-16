86b30d84dfa5f39d80181bcd1b2024dfcce2897b
Hanwen Zhang <hanwenzhang888@gmail.com>
2022-01-16 16:53:30 -0500

    Update 03-mongoose.js

--- MERN/03-mongoose.js
+++ MERN/03-mongoose.js
@@ -1,3 +1,13 @@
+//what is MongoDB?
+- a NoSQL database which stores documents in collections (instead of record in tables as in SQL)
+- stores application data (users, places)
+- enforces no data schema or relations
+- easily connected to node/express (not to react)
+- a powerful database which can easily be integrated into a node/express environment
+
+//What is Mongoose?
+- a popular library for MongoDB
+
 //app.js
 const express = require('express');
 const bodyParser = require('body-parser');
