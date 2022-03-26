333915b76f6e247e0a889b724b1eeacd75c84730
Hanwen Zhang <hanwenzhang888@gmail.com>
2022-03-26 18:44:58 -0400

    Create 05-metadata.js

---
+++ NextJS/05-metadata.js
@@ -0,0 +1,39 @@
+//import Head from 'next/head';
+//adding metadata and head elements to html
+//important for seo for showing up at search engines
+//you can do that to all the pages for the application
+
+import { Fragment } from 'react';
+import Head from 'next/head';
+
+import FeaturedPosts from '../components/home-page/featured-posts';
+import Hero from '../components/home-page/hero';
+import { getFeaturedPosts } from '../lib/posts-util';
+
+function HomePage(props) {
+  return (
+    <Fragment>
+      <Head>
+        <title>Blog</title>
+        <meta
+          name='description'
+          content='I post about programming and web development.'
+        />
+      </Head>
+      <Hero />
+      <FeaturedPosts posts={props.posts} />
+    </Fragment>
+  );
+}
+
+export function getStaticProps() {
+  const featuredPosts = getFeaturedPosts();
+
+  return {
+    props: {
+      posts: featuredPosts,
+    },
+  };
+}
+
+export default HomePage;
