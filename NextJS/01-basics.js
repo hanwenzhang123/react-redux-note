f1fb2516b7cc2fae770bd7a216d40be71aa3a551
Hanwen Zhang <hanwenzhang888@gmail.com>
2022-03-26 12:53:58 -0400

    Update 01-basics.js

--- NextJS/01-basics.js
+++ NextJS/01-basics.js
@@ -1,8 +1,40 @@
 #### What is Next.js
 - Next.js is a React framework for production created by Vercel (A fullstack framework for ReactJS)
-- Features you need for production: server-side rendering, static-site generation, API Routes, Typescript/Sass support, smart bundling, route pre-fetching, and more
+
+#### Key Features & Benefits
+- server-side rendering
+- static-site generation
+- File-based Routing
+- fullstack capabilities (backend api code using nodeJS)
+- Typescript/Sass support
+- smart bundling
+- route pre-fetching
 
 #### Server-side Rendering Next.js
 - NextJS allows the first page load to be rendered by the server, which is great for SEO & performance
 - unlike traditional React App where the entire application is loaded and rendered on the client
 
+
+initial setup
+npx create-next-app
+yarn create next-app
+  
+
+The React Framework for Production
+- React: you will write React code, you still build React components and use React features (props, state, context...)
+        NextJS just enhances your react app and adds more features
+- Framework: Lots of built-in features (e.g. routing) that help you solve common problems & clear guidance on how to use those features
+- Production: There are certain problems which you will need to solve for almost all production-ready React apps
+
+NextJS Key Features & Benefits
+- Server-side Rendering
+  Automatic page pre-rendering: Great for SEO and initial load
+  Blending client-side and server-side: fetch data on the server and render finished pages
+- File-based Routing
+  Define pages and routes with files and folders instead of code
+  Less code, less work, highly understandable
+- Fullstack Capabilities
+  Easily add backend (server-side) code to your Next/React apps
+  Storing data, getting data, authentication etc, can be added to your react projects
+  
+   
