3a6110b2aca52e9b8ff15bf20ce02e141144af25
Hanwen Zhang <hanwenzhang888@gmail.com>
2022-03-26 12:36:43 -0400

    Create 01-basics.js

---
+++ NextJS/01-basics.js
@@ -0,0 +1,8 @@
+#### What is Next.js
+- Next.js is a React framework for production created by Vercel (A fullstack framework for ReactJS)
+- Features you need for production: server-side rendering, static-site generation, API Routes, Typescript/Sass support, smart bundling, route pre-fetching, and more
+
+#### Server-side Rendering Next.js
+- NextJS allows the first page load to be rendered by the server, which is great for SEO & performance
+- unlike traditional React App where the entire application is loaded and rendered on the client
+
